66f7b8a62b5846b638cb33c63818eff22646a3c6
filipe campos <camplaix@gmail.com>
2017-02-26 18:27:52 +0000

    rework for LanteraGUI to draw after cosmetic terminal color changes

--- src/Server/Chat1/Messager.java
+++ src/Server/Chat1/Messager.java
@@ -5,7 +5,7 @@ package Server.Chat1;
  */
 public class Messager {
 
-    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RESET = " \u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_WHITE = "\u001B[37m";
     public static final String ANSI_RED = "\u001B[31m";
